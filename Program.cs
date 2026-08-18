ab481d4dc5dafc127da7a53e4460c88246f3c9d9
Said <saidig@code.edu.az>
2026-08-18 21:10:24 +0400

    task 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿#region Task1
+
+int[] array = { 5, 4, 6 };
+
+for (int i = 0; i < array.Length; i++) {
+    int factor = 1;
+    for (int j = 2; j <= array[i]; j++) {
+        factor *= j;
+    }
+
+    array[i] = factor;
+}
+
+foreach (var element in array) {
+    Console.Write($"{element} ");
+}
+
+Console.WriteLine();
+#endregion
