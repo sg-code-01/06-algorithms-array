d29837e238caa1bf552dd4f515f77dedd48e9046
Said <saidig@code.edu.az>
2026-08-19 19:26:51 +0400

    task 3

--- Program.cs
+++ Program.cs
@@ -19,31 +19,56 @@
 // #endregion
 // }}}
 // {{{ Task 2
-#region Task 2
+// #region Task 2
+//
+// int[] array = { 7, 12, 5, 8 };
+// int[] array_no_min = new int[array.Length - 1];
+// int min = array[0];
+//
+// for (int i = 1; i < array.Length; i++) {
+//     if (array[i] < min) {
+//         min = array[i];
+//     }
+// }
+//
+// int new_index = 0; // index of array without minimum element
+// for (int i = 0; i < array.Length; i++) {
+//     if (array[i] == min) {
+//         continue;
+//     }
+//     array_no_min[new_index] = array[i];
+//
+//     new_index++;
+// }
+//
+// foreach (var item in array_no_min) {
+//     Console.Write($"{item}, ");
+// }
+//
+// #endregion
+// }}}
+// Task 3 {{{
+#region Task 3
 
-int[] array = { 7, 12, 5, 8 };
-int[] array_no_min = new int[array.Length - 1];
-int min = array[0];
+string text = "kərtənkələ";
+char letter = ' ';
 
-for (int i = 1; i < array.Length; i++) {
-    if (array[i] < min) {
-        min = array[i];
+for (int i = 0; i < text.Length; i++) {
+    int counter = 0;
+
+    for (int j = 0; j < text.Length; j++) {
+        if (text[i] == text[j]) {
+            counter++;
+        }
+    }
+
+    if (counter == 1) {
+        letter = text[i];
+        break;
     }
 }
 
-int new_index = 0; // index of array without minimum element
-for (int i = 0; i < array.Length; i++) {
-    if (array[i] == min) {
-        continue;
-    }
-    array_no_min[new_index] = array[i];
-
-    new_index++;
-}
-
-foreach (var item in array_no_min) {
-    Console.Write($"{item}, ");
-}
+Console.WriteLine(letter);
 
 #endregion
 // }}}
