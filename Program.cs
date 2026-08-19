03b6b2cdb8fc956bb39545d4f485ce8f25b4781a
Said <saidig@code.edu.az>
2026-08-19 19:17:44 +0400

    task 2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,49 @@
-﻿#region Task1
+﻿// {{{ Task 1
+// #region Task1
+// int[] array = { 5, 4, 6 };
+//
+// for (int i = 0; i < array.Length; i++) {
+//     int factor = 1;
+//     for (int j = 2; j <= array[i]; j++) {
+//         factor *= j;
+//     }
+//
+//     array[i] = factor;
+// }
+//
+// foreach (var element in array) {
+//     Console.Write($"{element} ");
+// }
+//
+// Console.WriteLine();
+// #endregion
+// }}}
+// {{{ Task 2
+#region Task 2
 
-int[] array = { 5, 4, 6 };
+int[] array = { 7, 12, 5, 8 };
+int[] array_no_min = new int[array.Length - 1];
+int min = array[0];
 
-for (int i = 0; i < array.Length; i++) {
-    int factor = 1;
-    for (int j = 2; j <= array[i]; j++) {
-        factor *= j;
+for (int i = 1; i < array.Length; i++) {
+    if (array[i] < min) {
+        min = array[i];
     }
-
-    array[i] = factor;
 }
 
-foreach (var element in array) {
-    Console.Write($"{element} ");
+int new_index = 0; // index of array without minimum element
+for (int i = 0; i < array.Length; i++) {
+    if (array[i] == min) {
+        continue;
+    }
+    array_no_min[new_index] = array[i];
+
+    new_index++;
 }
 
-Console.WriteLine();
+foreach (var item in array_no_min) {
+    Console.Write($"{item}, ");
+}
+
 #endregion
+// }}}
